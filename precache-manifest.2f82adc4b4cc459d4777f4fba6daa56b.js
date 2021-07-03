995c93b20f60a402a93b38524a7a321a75fd06ad
JJ-Dev18 <jj.murillo182020@gmail.com>
2021-07-02 21:47:46 -0500

    Updates

---
+++ precache-manifest.2f82adc4b4cc459d4777f4fba6daa56b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1257330ae556b98c80ce8d0a9519f9ad",
+    "url": "/JournalEntries/index.html"
+  },
+  {
+    "revision": "5d4209a8d330f5f679ba",
+    "url": "/JournalEntries/static/css/main.53a3af32.chunk.css"
+  },
+  {
+    "revision": "0921983f223fb2fe437b",
+    "url": "/JournalEntries/static/js/2.8defc4ea.chunk.js"
+  },
+  {
+    "revision": "d0b13a04dd0dc4c851ad16fad1cc6f51",
+    "url": "/JournalEntries/static/js/2.8defc4ea.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5d4209a8d330f5f679ba",
+    "url": "/JournalEntries/static/js/main.cbd9f5f8.chunk.js"
+  },
+  {
+    "revision": "901c2d4c11bcebd20c75",
+    "url": "/JournalEntries/static/js/runtime-main.76e2a0aa.js"
+  }
+]);
